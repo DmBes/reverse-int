0e84aea0f428f41b19816e79a1c05ac839451418
Дмитрий Беспалов <b-192@tut.by>
2022-02-24 21:20:49 +0300

    Finish

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+    n =  n< 0 ? n * -1 : n;
+    let str = '' + n
+    let result = ''
+
+    for (let i = str.length - 1; 0 <= i ; i--) {
+        result += str[i]
+    }
+    return +result;
 }
